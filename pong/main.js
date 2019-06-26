39ccf1e93ebc635e722dd5fcdbea2198b5ec026e
Will Constable <willconstable@gmail.com>
2019-06-25 21:56:48 -0700

    groundwork for pong

---
+++ pong/main.js
@@ -0,0 +1,204 @@
+/* eslint no-console:0 consistent-return:0 */
+"use strict";
+
+// Globals
+var program;
+var vertices = [];
+
+// Returns a random integer from 0 to range - 1.
+function randomInt(range) {
+  return Math.floor(Math.random() * range);
+}
+
+function randomFloat(){
+  return Math.random()*2.0 - 1.0;
+}
+
+// Fills the buffer with the values that define a rectangle.
+function setRectangle(gl, x, y, width, height) {
+  var x1 = x;
+  var x2 = x + width;
+  var y1 = y;
+  var y2 = y + height;
+ 
+  // NOTE: gl.bufferData(gl.ARRAY_BUFFER, ...) will affect
+  // whatever buffer is bound to the `ARRAY_BUFFER` bind point
+  // but so far we only have one buffer. If we had more than one
+  // buffer we'd want to bind that buffer to `ARRAY_BUFFER` first.
+ 
+  vertices.push.apply(vertices, [
+     x1, y1,
+     x2, y1,
+     x1, y2,
+     x1, y2,
+     x2, y1,
+     x2, y2])
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+}
+
+function onClickNewGame() {
+  var canvas = document.getElementById("display");
+  var gl = canvas.getContext("webgl");
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  vertices = [];
+}
+
+function onClickLeft() {
+  old();
+}
+
+function onClickRight() {
+
+
+}
+
+function old(){
+  // Get A WebGL context
+  var canvas = document.getElementById("display");
+  var gl = canvas.getContext("webgl");
+  if (!gl) {
+    return;
+  }
+  // Setup a random rectangle
+  // This will write to positionBuffer because
+  // its the last thing we bound on the ARRAY_BUFFER
+  // bind point
+  setRectangle(
+      gl, randomFloat(), randomFloat(), randomFloat(), randomFloat());
+
+  // Set a random color.
+  var colorUniformLocation = gl.getUniformLocation(window.program, "u_color");
+  gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1);
+
+  // Draw the rectangle.
+  gl.drawArrays(gl.TRIANGLES, 0, vertices.length/2);
+}
+
+// use convention position=topleft
+function drawPaddle(gl, x, y) {
+  var width = 0.15;
+  var height = 0.05;
+  setRectangle(gl, x, y, width, height);
+
+  // Set a random color.
+  var colorUniformLocation = gl.getUniformLocation(window.program, "u_color");
+  gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1);
+
+  // Draw the rectangle.
+  gl.drawArrays(gl.TRIANGLES, 0, vertices.length/2);
+}
+
+function drawBall(gl, x, y) {
+  var width = 0.02;
+  var height = 0.04;
+  setRectangle(gl, x, y, width, height);
+
+  // Set a random color.
+  var colorUniformLocation = gl.getUniformLocation(window.program, "u_color");
+  gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1);
+
+  // Draw the rectangle.
+  gl.drawArrays(gl.TRIANGLES, 0, vertices.length/2);
+}
+
+function createShader(gl, type, source) {
+  var shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (success) {
+    return shader;
+  }
+
+  console.log(gl.getShaderInfoLog(shader));
+  gl.deleteShader(shader);
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+  var program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (success) {
+    return program;
+  }
+
+  console.log(gl.getProgramInfoLog(program));
+  gl.deleteProgram(program);
+}
+
+function main() {
+  // Get A WebGL context
+  var canvas = document.getElementById("display");
+  var gl = canvas.getContext("webgl");
+  if (!gl) {
+    return;
+  }
+
+  // Get the strings for our GLSL shaders
+  var vertexShaderSource = document.getElementById("2d-vertex-shader").text;
+  var fragmentShaderSource = document.getElementById("2d-fragment-shader").text;
+
+  // create GLSL shaders, upload the GLSL source, compile the shaders
+  var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+  var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+  // Link the two shaders into a program
+  program = createProgram(gl, vertexShader, fragmentShader);
+
+  // look up where the vertex data needs to go.
+  var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+
+  // Create a buffer and put three 2d clip space points in it
+  var positionBuffer = gl.createBuffer();
+
+  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+
+  // code above this line is initialization code.
+  // code below this line is rendering code.
+
+  webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+
+  // Tell WebGL how to convert from clip space to pixels
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+  // Clear the canvas
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // Tell it to use our program (pair of shaders)
+  gl.useProgram(program);
+
+  // Turn on the attribute
+  gl.enableVertexAttribArray(positionAttributeLocation);
+  var colorUniformLocation = gl.getUniformLocation(program, "u_color");
+  gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1);
+
+  // Bind the position buffer.
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+  var size = 2;          // 2 components per iteration
+  var type = gl.FLOAT;   // the data is 32bit floats
+  var normalize = false; // don't normalize the data
+  var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+  var offset = 0;        // start at the beginning of the buffer
+  gl.vertexAttribPointer(
+      positionAttributeLocation, size, type, normalize, stride, offset);
+
+
+  drawPaddle(gl, -0.15/2, -1);
+  drawPaddle(gl, -0.15/2, 1-0.05);
+  drawBall(gl, -0.01/2, -0.02/2);
+
+
+  // draw
+  var primitiveType = gl.TRIANGLES;
+  var offset = 0;
+  gl.drawArrays(primitiveType, offset, vertices.length/2);
+}
+
+main();
